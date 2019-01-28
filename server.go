279a5e4ea528fddd9f4e57508188be608b2cb168
DimosthenisBotsaris <botsaris.d@gmail.com>
2019-01-28 14:20:32 +0200

    Add gitignore

--- server.go
+++ server.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/eldimious/golang-api-showcase/config"
 	postgres "github.com/eldimious/golang-api-showcase/data/database"
 )
 
 func main() {
-	configuration := config.NewConfig()
+	configuration, err := config.NewConfig()
 	if err != nil {
 		panic(err)
 	}
+
 	postgres, err := postgres.Open(configuration.Postgres)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(postgres)
 }
